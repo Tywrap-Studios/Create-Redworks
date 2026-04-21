92bc96dc624a79414eb30b337bbc2a633a51eb33
ItsTiazzz <163735662+ItsTiazzz@users.noreply.github.com>
2026-04-21 15:01:33 +0200

    Remove imports
    Took 7 minutes

--- src/main/kotlin/org/tywrapstudios/redwork/Redwork.kt
+++ src/main/kotlin/org/tywrapstudios/redwork/Redwork.kt
@@ -1,8 +1,6 @@
 package org.tywrapstudios.redwork
 
 import com.simibubi.create.foundation.data.CreateRegistrate
-import com.tterrag.registrate.util.entry.ItemEntry
-import net.minecraft.world.item.Item
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.tywrapstudios.redwork.platform.ModPlatform
